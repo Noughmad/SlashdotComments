a06b2967bd5c96e06b82fa25bee7ad9f42488a57
Miha ?an?ula <miha@noughmad.eu>
2013-08-03 17:48:58 +0200

    Apparently, the quote is updated every hour

--- src/com/noughmad/plusfive/SlashdotProvider.java
+++ src/com/noughmad/plusfive/SlashdotProvider.java
@@ -70,7 +70,6 @@ public class SlashdotProvider extends ContentProvider {
         Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(millis);
 
-        cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
